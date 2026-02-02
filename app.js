2268e317227eadea306a73326e36bc9119e25770
polymorpheteen <138623315+polymorpheteen@users.noreply.github.com>
2026-02-02 17:21:37 +0100

    Update the dashboard page

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: false,
       sameSite: "lax",
       maxAge: 1000 * 60 * 60 * 24 * 7,
     },
